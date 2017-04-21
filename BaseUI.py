ac02e6b9e1f9575cca65eb21b5d917bd48ec5f68
jnlasher <jnlasher@gmail.com>
2017-04-21 16:58:43 -0400

    v0.0.1
    Allowed list updates

--- BaseUI.py
+++ BaseUI.py
@@ -6,21 +6,29 @@ class Application():
     def __init__(self, master):
         self.master = master
         master.title('PyRest')
+        master.minsize(width=300, height=100)
 
         self.requestType = tk.StringVar()
         self.requestType.set('GET')
+        self.requestType.trace("w", self.UpdateRequestType)
         self.requestOption = tk.OptionMenu(master, self.requestType, 'GET', 'POST', 'PUT', 'DELETE')
         self.requestOption.pack()
+
+        self.addressLabel = tk.Entry(master)
+        self.addressLabel.pack()
 
         self.close = tk.Button(master, text='Close', command=self.Close)
         self.close.pack()
 
-    def UpdateRequestType(self):
+    def UpdateRequestType(self, *args):
         print('Request type is: {}'.format(self.requestType.get()))
+
+    def GetURI(self):
+        print('url is: {}'.format(self.addressLabel.get()))
 
     def Close(self):
         self.master.quit()
-        print('Request type is: {}'.format(self.requestType.get()))
+
 
 root = tk.Tk()
 app = Application(root)
