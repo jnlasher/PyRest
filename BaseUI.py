ae95833413d714b33395d82c75198cf82803a0f6
jnlasher <jnlasher@gmail.com>
2017-04-25 22:46:48 -0400

    v0.0.4
    Updated to use grid. Cleaned interface.

--- BaseUI.py
+++ BaseUI.py
@@ -1,79 +1,70 @@
 import tkinter as tk
 from tkinter import ttk
-import requests
 
 
 class Application:
     def __init__(self, master):
-        # SETUP
+        #SETUP
         self.master = master
-        master.title('PyRest')
+        master.title('PyREST')
         master.minsize(width=550, height=250)
+        self.c_row = 1
         val = tk.IntVar()
 
-        # GENERATE LAYOUT
-        top = tk.Frame(master)
-        bottom = tk.Frame(master)
-        top.pack(side=tk.TOP)
-        bottom.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-        # LAYOUT DIVISIONS
-        divs = ttk.Notebook(master)
-        self.headerPage = ttk.Frame(divs, width = 300, height=100)
-        self.bodyPage = ttk.Frame(divs, width = 300, height=100)
+        # GENERATE FRAMES
+        topFrame = tk.Frame(master, bg='cyan', width = 450, height=50, padx=30, pady=3)
+        centerFrame = tk.Frame(master, bg='gray2', width=50, height=40, padx=30, pady=3)
+        responseFrame = tk.Frame(master, bg='white', width = 450, height = 45, padx=30, pady=3)
+        topFrame.grid_propagate(False)
+        topFrame.grid(row=0, sticky='ew')
+        centerFrame.grid(row=1, sticky='ew')
+        responseFrame.grid(row=3, sticky='ew')
+        divs = ttk.Notebook(centerFrame)
+        self.headerPage = ttk.Frame(divs)
+        self.bodyPage = ttk.Frame(divs)
         divs.add(self.headerPage, text='Headers')
         divs.add(self.bodyPage, text='Body')
-        divs.grid(column=0)
-        divs.pack(fill=tk.X, padx=30)
 
         # CREATE WIDGETS
         self.requestType = tk.StringVar()
         self.requestType.set('GET')
-        self.requestType.trace("w", self.UpdateRequestType)
-        self.requestOption = tk.OptionMenu(master, self.requestType, 'GET', 'POST', 'PUT', 'DELETE')
-        self.requestOption.pack(in_=top, side=tk.LEFT)
+        self.requestType.trace('w', self.UpdateRequestType)
+        self.requestOption = tk.OptionMenu(topFrame, self.requestType, 'GET','POST','PUT','DELETE')
+        self.addressLabel = tk.Entry(topFrame, width=50)
+        # First Division
+        self.headersKey = tk.Entry(self.headerPage)
+        self.headersVal = tk.Entry(self.headerPage, width=39)
+        self.addHButton = tk.Button(self.headerPage, text = 'Add Header', command = self.AddHeader)
+        # Split
+        self.formData = tk.Radiobutton(self.bodyPage, text='form-data', variable=val, value=1)
+        self.urlEncoded = tk.Radiobutton(self.bodyPage, text='x-www-form-urlencoded', variable=val, value=2)
+        self.rawData = tk.Radiobutton(self.bodyPage, text='raw', variable=val, value=3)
+        self.binaryFile = tk.Radiobutton(self.bodyPage, text='binary', variable=val, value=4)
 
-        self.addressLabel = tk.Entry(master)
-        self.addressLabel.pack(in_=top, side=tk.LEFT)
-        # Build the headers subsection
-        self.headersLabel = tk.Entry(self.headerPage)
-        self.headersLabel.pack(fill=tk.X, padx=30, pady=6)
-        self.addHButton = tk.Button(self.headerPage, text = 'Add Header', command= self.AddHeader)
-        self.addHButton.pack(side=tk.BOTTOM)
-        # Build the body/message subsection
-        self.formData = tk.Radiobutton(self.bodyPage, text='form-data', variable=val, value=1).pack(side=tk.LEFT)
-        self.urlEncoded = tk.Radiobutton(self.bodyPage, text='x-www-form-urlencoded', variable=val, value=2).pack(side=tk.LEFT)
-        self.rawData = tk.Radiobutton(self.bodyPage, text='raw', variable=val, value=3).pack(side=tk.LEFT)
-        self.binaryFile = tk.Radiobutton(self.bodyPage, text='binary', variable=val, value=4).pack(side=tk.LEFT)
+        # CREATE LAYOUT
+        self.requestOption.grid(row=0, column=0, padx=3)
+        self.addressLabel.grid(row=0, column=1, padx=3)
+        divs.grid()
+        self.headersKey.grid(row=0, column=0, padx=3)
+        self.headersVal.grid(row=0, column=1, padx=3)
+        self.addHButton.grid(row=1, column=0)
+        # Split
+        self.formData.grid(row=0, column=0)
+        self.urlEncoded.grid(row=0, column=1)
+        self.rawData.grid(row=0, column=2)
+        self.binaryFile.grid(row=0, column=3)
 
-        self.close = tk.Button(master, text='Submit', command=self.Close)
-        self.close.pack(in_=bottom)
-
-
-#        self.responseBox = tk.Text(master, width=25, height=10)
-#        scrollbar = tk.Scrollbar(master)
-#        scrollbar.config(command=self.responseBox.yview)
-#        self.responseBox.config(yscrollcommand=scrollbar.set)
-#        scrollbar.pack(in_=bottom, side=tk.RIGHT, fill=tk.Y)
-#        self.responseBox.pack(in_=bottom, side=tk.LEFT, fill=tk.BOTH, expand=True)
 
     def AddHeader(self):
-        newEntry = tk.Entry(self.headerPage)
-        newEntry.pack(side=tk.TOP, fill=tk.X, padx=30)
-        self.addHButton.pack()
-
-    def AddBody(self):
-        newEntry = tk.Entry(self.bodyPage)
-        newEntry.pack(side=tk.BOTTOM, fill=tk.X, padx=30)
-        self.addBButton.pack(side=tk.BOTTOM)
+        newKey = tk.Entry(self.headerPage)
+        newVal = tk.Entry(self.headerPage, width=39)
+        newKey.grid(row=self.c_row, column = 0)
+        newVal.grid(row=self.c_row, column = 1)
+        self.addHButton.grid(row=self.c_row+1, column=0)
+        self.c_row += 1
 
     def UpdateRequestType(self, *args):
         print('Request type is: {}'.format(self.requestType.get()))
-
-    def GetURI(self):
-        print('url is: {}'.format(self.addressLabel.get()))
-
-    def Close(self):
-        self.master.quit()
 
 
 root = tk.Tk()
